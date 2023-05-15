6fb9a4fac91fd6b029b854cdec073b0b68119b31
Mujahid Raj <113965511+mujahidraj@users.noreply.github.com>
2023-05-15 23:22:31 +0600

    Add files via upload

---
+++ Aboutdeveloper.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace HOTELMANAGEMENTSYSTEM
+{
+    partial class Aboutdeveloper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Aboutdeveloper));
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            pictureBox2 = new PictureBox();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            pictureBox3 = new PictureBox();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            pictureBox4 = new PictureBox();
+            label14 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Showcard Gothic", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(243, 17);
+            label1.Name = "label1";
+            label1.Size = new Size(274, 27);
+            label1.TabIndex = 0;
+            label1.Text = "About the developer ";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Location = new Point(327, 69);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(108, 96);
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = SystemColors.ButtonFace;
+            label2.Location = new Point(306, 168);
+            label2.Name = "label2";
+            label2.Size = new Size(150, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Nazmus Shakib Shan ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.ForeColor = SystemColors.ButtonFace;
+            label3.Location = new Point(316, 188);
+            label3.Name = "label3";
+            label3.Size = new Size(119, 15);
+            label3.TabIndex = 3;
+            label3.Text = "Course Co-ordinator ";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.ForeColor = SystemColors.ButtonFace;
+            label4.Location = new Point(288, 203);
+            label4.Name = "label4";
+            label4.Size = new Size(177, 15);
+            label4.TabIndex = 4;
+            label4.Text = "Object Oriented Programming 2";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackgroundImage = (Image)resources.GetObject("pictureBox2.BackgroundImage");
+            pictureBox2.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox2.Location = new Point(70, 238);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(109, 106);
+            pictureBox2.TabIndex = 5;
+            pictureBox2.TabStop = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.ForeColor = SystemColors.ButtonFace;
+            label5.Location = new Point(51, 384);
+            label5.Name = "label5";
+            label5.Size = new Size(153, 15);
+            label5.TabIndex = 8;
+            label5.Text = "Design of Kitchen and Ideas";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.Transparent;
+            label6.ForeColor = SystemColors.ButtonFace;
+            label6.Location = new Point(92, 369);
+            label6.Name = "label6";
+            label6.Size = new Size(65, 15);
+            label6.TabIndex = 7;
+            label6.Text = "22-46413-1";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.Transparent;
+            label7.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.ForeColor = SystemColors.ButtonFace;
+            label7.Location = new Point(52, 349);
+            label7.Name = "label7";
+            label7.Size = new Size(152, 20);
+            label7.TabIndex = 6;
+            label7.Text = "Asmaul Husna Mayad";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.BackColor = Color.Transparent;
+            label8.ForeColor = SystemColors.ButtonFace;
+            label8.Location = new Point(307, 384);
+            label8.Name = "label8";
+            label8.Size = new Size(157, 15);
+            label8.TabIndex = 12;
+            label8.Text = "Backend and Frontend of All";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.BackColor = Color.Transparent;
+            label9.ForeColor = SystemColors.ButtonFace;
+            label9.Location = new Point(348, 369);
+            label9.Name = "label9";
+            label9.Size = new Size(65, 15);
+            label9.TabIndex = 11;
+            label9.Text = "22-46398-1";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.BackColor = Color.Transparent;
+            label10.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label10.ForeColor = SystemColors.ButtonFace;
+            label10.Location = new Point(246, 349);
+            label10.Name = "label10";
+            label10.Size = new Size(271, 20);
+            label10.TabIndex = 10;
+            label10.Text = "Moizuddin Mohammad Mujahid Rashid";
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackgroundImage = (Image)resources.GetObject("pictureBox3.BackgroundImage");
+            pictureBox3.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox3.Location = new Point(326, 238);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(109, 106);
+            pictureBox3.TabIndex = 9;
+            pictureBox3.TabStop = false;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.BackColor = Color.Transparent;
+            label11.ForeColor = SystemColors.ButtonFace;
+            label11.Location = new Point(574, 384);
+            label11.Name = "label11";
+            label11.Size = new Size(147, 15);
+            label11.TabIndex = 16;
+            label11.Text = "About us Design and Ideas";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.BackColor = Color.Transparent;
+            label12.ForeColor = SystemColors.ButtonFace;
+            label12.Location = new Point(615, 369);
+            label12.Name = "label12";
+            label12.Size = new Size(65, 15);
+            label12.TabIndex = 15;
+            label12.Text = "22-46390-1";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.BackColor = Color.Transparent;
+            label13.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label13.ForeColor = SystemColors.ButtonFace;
+            label13.Location = new Point(575, 349);
+            label13.Name = "label13";
+            label13.Size = new Size(122, 20);
+            label13.TabIndex = 14;
+            label13.Text = "Farhanaz Kamrun";
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackgroundImage = (Image)resources.GetObject("pictureBox4.BackgroundImage");
+            pictureBox4.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox4.Location = new Point(593, 238);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(109, 106);
+            pictureBox4.TabIndex = 13;
+            pictureBox4.TabStop = false;
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.BackColor = Color.Transparent;
+            label14.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label14.ForeColor = Color.White;
+            label14.Location = new Point(721, 17);
+            label14.Name = "label14";
+            label14.Size = new Size(56, 25);
+            label14.TabIndex = 17;
+            label14.Text = "Back ";
+            label14.Click += label14_Click;
+            // 
+            // Aboutdeveloper
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(800, 450);
+            Controls.Add(label14);
+            Controls.Add(label11);
+            Controls.Add(label12);
+            Controls.Add(label13);
+            Controls.Add(pictureBox4);
+            Controls.Add(label8);
+            Controls.Add(label9);
+            Controls.Add(label10);
+            Controls.Add(pictureBox3);
+            Controls.Add(label5);
+            Controls.Add(label6);
+            Controls.Add(label7);
+            Controls.Add(pictureBox2);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Name = "Aboutdeveloper";
+            Text = "Aboutdeveloper";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private PictureBox pictureBox2;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private PictureBox pictureBox3;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private PictureBox pictureBox4;
+        private Label label14;
+    }
+}
---
+++ Aboutdeveloper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HOTELMANAGEMENTSYSTEM
+{
+    public partial class Aboutdeveloper : Form
+    {
+        public Aboutdeveloper()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+            Homepage hm = new Homepage();
+            hm.Show();
+        }
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Homepage homepage = new Homepage();
+            homepage.Show();
+        }
+    }
+}
